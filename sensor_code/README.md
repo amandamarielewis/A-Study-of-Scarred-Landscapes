4ade82592e6fff141e842dd7ed38a00263433f3b
amandamarielewis <lewia184@newschool.edu>
2017-12-04 18:49:47 +0100

    update dec4

---
+++ sensor_code/README.md
@@ -0,0 +1,3 @@
+# Arduino Code for instrument
+
+![Dust Sensor User Manual](https://www.waveshare.com/w/upload/0/0a/Dust-Sensor-User-Manual-EN.pdf)
--- sensor_code/sensor_code.ino
+++ sensor_code/sensor_code.ino
@@ -1,3 +1,18 @@
+#include <Adafruit_BME280.h>
+
+//code for Amanda Lewis thesis 2017
+//presently includes code for MQ-5, MQ-2, gy-bmp280, and dust sensor
+
+//include lib for adafruit bmp280 sensor
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+
+#define BME_SCK 13
+#define BME_MISO 12
+#define BME_MOSI 11
+#define BME_CS 10
+Adafruit_BME280 bme; // I2C
 
 // include the lcd library code:
 #include <LiquidCrystal.h>
@@ -5,154 +20,207 @@
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(3,4,5,6,7,8);
 
+//temp + huminidy
+float temp, pressure, altitude;
+
+//define parameters for dust sensor
+#define        COV_RATIO                       0.2            //ug/mmm / mv
+#define        NO_DUST_VOLTAGE                 400            //mv
+#define        SYS_VOLTAGE                     5000       
+
+float density, voltage;
+int   adcvalue;
+const int iled = 9;                                            //drive the led of sensor
+const int vout = 1;                                            //analog input
+
 //for the mq-2 sensor:
-int redLed = 12;
-int greenLed = 11;
-int buzzer = 10;
-int smokeA0 = A5;
-// Your threshold value
-int sensorThres = 400;
-int potPin1 = smokeA0;
+float ratio2;
+
+//for the mq-5 sensor:
+float ratio5;
 
 void setup() {
-  pinMode(redLed, OUTPUT);
-  pinMode(greenLed, OUTPUT);
-  pinMode(buzzer, OUTPUT);
-  pinMode(smokeA0, INPUT);
-  Serial.begin(9600);
-
+   //  dust sensor
+    pinMode(iled, OUTPUT);
+    digitalWrite(iled, LOW);                                     //iled default closed
+    
   // set up the LCD's number of columns and rows:
-  lcd.begin(16, 2);
-
-
+    lcd.begin(16, 2);  
+    Serial.begin(9600);                                         //send and receive at 9600 baud
+  
 }
 
 void loop() {
 
     MQ2();
     MQ5();
+    dust();
+    bmp280();
     
 //to print on lcd screen:
-//    lcd.clear();
-//    lcd.setCursor(0,0); 
-//    lcd.print("SensorVal1: "); 
-//    lcd.print(ratio); 
-//    lcd.setCursor(0,1); 
+    lcd.clear();
+    lcd.setCursor(0,0); 
+      lcd.print("MQ2: "); 
+      lcd.print(ratio2); 
+    lcd.setCursor(0,1); 
+      lcd.print("MQ5: "); 
+      lcd.print(ratio5); 
+      
+delay(2000);
+
+//  lcd.clear();
+    lcd.setCursor(0,0); 
+      lcd.print("Dust: ");
+      lcd.print(density);
+      lcd.print(" ug/m3\n");  
+    lcd.setCursor(0,1); 
+      lcd.print("Temp: ");
+      lcd.print(temp);
+      lcd.println(" *C");
+  delay(2000);
 
 }
 
+/*-----------------------------------------------------------------------*/
 void MQ2(){
   //MQ-2 Gas Sensor Code  
   /*-Replace the value with a value from the test of c02 levels in clean area -*/
     float R0 = 0.35;
     float sensor_volt;
     float RS_gas; // Get value of RS in a GAS
-    float ratio; // Get ratio RS_GAS/RS_air
-    int sensorValue = analogRead(smokeA0);
+    int sensorValue = analogRead(A3);
     sensor_volt=(float)sensorValue/1024*5.0;
     RS_gas = (5.0-sensor_volt)/sensor_volt; // omit *RL
-    ratio = RS_gas/R0;  // ratio = RS/R0
+    ratio2 = RS_gas/R0;  // ratio = RS/R0
           
+//    Serial.print("sensor_volt = ");
+//    Serial.println(sensor_volt);
+//    Serial.print("RS_ratio = ");
+//    Serial.println(RS_gas);
+//    Serial.print("Rs/R0 = ");
+    Serial.println("MQ2: ");
+    Serial.println(ratio2);
+    Serial.print("\n\n");
+
+
+  }
+
+/*-----------------------------------------------------------------------*/
+void MQ5(){
+    float R0 = 1;
+    float sensor_volt;
+    float RS_gas; // Get value of RS in a GAS
+//    float ratio5; // Get ratio RS_GAS/RS_air
+    int sensorValue = analogRead(A0);
+    sensor_volt=(float)sensorValue/1024*5.0;
+    RS_gas = (5.0-sensor_volt)/sensor_volt; // omit *RL
+
+          /*-Replace the name "R0" with the value of R0 in the demo of First Test -*/
+    ratio5 = RS_gas/R0;  // ratio = RS/R0
+          /*-----------------------------------------------------------------------*/
 
 //    Serial.print("sensor_volt = ");
 //    Serial.println(sensor_volt);
 //    Serial.print("RS_ratio = ");
 //    Serial.println(RS_gas);
 //    Serial.print("Rs/R0 = ");
-    Serial.println(ratio);
-//    Serial.print("\n\n");
+    Serial.println("MQ5: ");
+    Serial.println(ratio5);
+    Serial.print("\n\n");
+  
+  }
 
-    delay(1000);
+/*-----------------------------------------------------------------------*/
+void dust(){
 
-//  Serial.print("Pin A0: ");
-//  Serial.println(sensorValue);
-  // Checks if it has reached the threshold value
-  if (sensorValue > sensorThres)
+//  get adcvalue
+
+  digitalWrite(iled, HIGH);
+  delayMicroseconds(280);
+  adcvalue = analogRead(vout);
+  digitalWrite(iled, LOW);
+  
+  adcvalue = Filter(adcvalue);
+  
+  
+//  covert voltage (mv)
+  voltage = (SYS_VOLTAGE / 1024.0) * adcvalue * 11;
+  
+//  voltage to density
+
+  if(voltage >= NO_DUST_VOLTAGE)
   {
-    digitalWrite(redLed, HIGH);
-    digitalWrite(greenLed, LOW);
-    tone(buzzer, 1000, 200);
+    voltage -= NO_DUST_VOLTAGE;
+    
+    density = voltage * COV_RATIO;
+  }
+  else
+    density = 0;
+    
+  /*
+  display the result
+  */
+  Serial.print("**********************************************************************\n");
+  Serial.print("The current dust concentration is: ");
+  Serial.print(density);
+  Serial.print(" ug/m3\n");  
+  
+
+  }  
+/*-----------------------------------------------------------------------*/
+void bmp280(){
+    temp = bme.readTemperature();
+    pressure = bme.readPressure() / 100; // 100 Pa = 1 millibar
+//    altitude = bme.readHumidity(1013.25);
+    
+    Serial.print("---- GY BMP 280 ----------------\n");
+    Serial.print("Temperature = ");
+    Serial.print(temp);
+    Serial.println(" *C");
+//    Serial.print("Pressure = ");
+//    Serial.print(pressure); // 100 Pa = 1 millibar
+//    Serial.println(" mb");
+//    Serial.print("Approx altitude = ");
+//    Serial.print(altitude);
+//    Serial.println(" m");
+//    Serial.print("--------------------------------\n\n");
+//    delay(2000);
+
+  }
+  
+
+/*
+private function used for dust sensor
+*/
+int Filter(int m)
+{
+  static int flag_first = 0, _buff[10], sum;
+  const int _buff_max = 10;
+  int i;
+  
+  if(flag_first == 0)
+  {
+    flag_first = 1;
+
+    for(i = 0, sum = 0; i < _buff_max; i++)
+    {
+      _buff[i] = m;
+      sum += _buff[i];
+    }
+    return m;
   }
   else
   {
-    digitalWrite(redLed, LOW);
-    digitalWrite(greenLed, HIGH);
-    noTone(buzzer);
-  }
-  delay(100);
-
-//write values for this sensor
-    lcd.clear();
-    lcd.setCursor(0,0); 
-    lcd.print("SensorVal1: "); 
-    lcd.print(ratio); 
-
-  
-  }
-
-void MQ5(){
-    float R0 = 1;
-    float sensor_volt;
-    float RS_gas; // Get value of RS in a GAS
-    float ratio; // Get ratio RS_GAS/RS_air
-    int sensorValue = analogRead(A0);
-    sensor_volt=(float)sensorValue/1024*5.0;
-    RS_gas = (5.0-sensor_volt)/sensor_volt; // omit *RL
-
-          /*-Replace the name "R0" with the value of R0 in the demo of First Test -*/
-    ratio = RS_gas/R0;  // ratio = RS/R0
-          /*-----------------------------------------------------------------------*/
-
-    Serial.print("sensor_volt = ");
-    Serial.println(sensor_volt);
-    Serial.print("RS_ratio = ");
-    Serial.println(RS_gas);
-    Serial.print("Rs/R0 = ");
-    Serial.println(ratio);
-
-    Serial.print("\n\n");
-
-    delay(1000);
-
+    sum -= _buff[0];
+    for(i = 0; i < (_buff_max - 1); i++)
+    {
+      _buff[i] = _buff[i + 1];
+    }
+    _buff[9] = m;
+    sum += _buff[9];
     
-//lcd.clear();
-  lcd.setCursor(0,1); 
-  lcd.print("Sensor2: ");
-  lcd.print(ratio);
-  
+    i = sum / 10.0;
+    return i;
   }
+}
 
-
-
-//code to get the RO
-//
-//void setup() {
-//    Serial.begin(9600);
-//}
-//
-//void loop() {
-//    float sensor_volt;
-//    float RS_air; //  Get the value of RS via in a clear air
-//    float R0;  // Get the value of R0 via in H2
-//    float sensorValue;
-//
-//        /*--- Get a average data by testing 100 times ---*/
-//    for(int x = 0 ; x < 100 ; x++)
-//    {
-//        sensorValue = sensorValue + analogRead(A0);
-//    }
-//    sensorValue = sensorValue/100.0;
-//        /*-----------------------------------------------*/
-//
-//    sensor_volt = sensorValue/1024*5.0;
-//    RS_air = (5.0-sensor_volt)/sensor_volt; // omit *RL
-//    R0 = RS_air/6.5; // The ratio of RS/R0 is 6.5 in a clear air from Graph (Found using WebPlotDigitizer)
-//
-//    Serial.print("sensor_volt = ");
-//    Serial.print(sensor_volt);
-//    Serial.println("V");
-//
-//    Serial.print("R0 = ");
-//    Serial.println(R0);
-//    delay(1000);
-//}
